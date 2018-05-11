c2d8f5cd04a91a8dce094a010638117a57d944dd
Sander Bollen <a.h.b.bollen@lumc.nl>
2018-05-11 14:34:13 +0200

    add info fields

--- aav/variation.py
+++ aav/variation.py
@@ -6,7 +6,40 @@ aav.variation
 :copyright: (c) 2018 Leiden University Medical Center
 :license: MIT
 """
-from typing import List, Optional
+import enum
+from typing import List, Optional, Any
+
+
+class InfoFieldNumber(enum.Enum):
+    one = "1"
+    A = "A"
+    R = "R"
+    G = "G"
+    unknown = "."
+
+
+class InfoField(object):
+    """Info field"""
+
+    def __init__(self, name: str, value: Any,
+                 number: InfoFieldNumber, flag: bool = False):
+        if flag and not isinstance(value, bool):
+            raise ValueError("Value must be boolean if a flag")
+
+        self.name = name
+        self.value = value
+        self.number = number
+        self.flag = flag
+
+    def __str__(self) -> Optional[str]:
+        if self.flag and self.value:
+            return self.name
+        elif self.flag:
+            return ""
+        elif self.number == InfoFieldNumber.one:
+            return "{0}={1}".format(self.name, self.value)
+        else:
+            return "{0}={1}".format(self.name, ",".join(map(str, self.value)))
 
 
 class Variant(object):
@@ -17,7 +50,8 @@ class Variant(object):
     """
     def __init__(self, chrom: str, pos: int, ref: str, alt: List[str],
                  qual: float, filters: List[str] = list(),
-                 id: Optional[str] = None):
+                 id: Optional[str] = None,
+                 info_fields: List[InfoField] = list()):
         self.chrom = chrom
         self.pos = pos
         if id is not None:
@@ -28,13 +62,20 @@ class Variant(object):
         self.ref = ref
         self.alt = alt
         self.filters = filters
+        self.info_fields = info_fields
 
     @property
-    def vcf_line(self):
+    def vcf_line(self) -> str:
         fmt = "{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}".format(
             self.chrom, self.pos, self.id,
             self.ref, ",".join(self.alt),
             self.qual,
             ",".join(self.filters) if len(self.filters) > 0 else "PASS"
         )
+        if len(self.info_fields) > 0:
+            info_str = ";".join(
+                str(x) for x in self.info_fields if str(x) != ""
+            )
+            fmt += "\t{0}".format(info_str)
+
         return fmt
--- tests/test_variation.py
+++ tests/test_variation.py
@@ -7,7 +7,51 @@ test_variation.py
 :license: MIT
 """
 import pytest
-from aav.variation import Variant
+from aav.variation import Variant, InfoField, InfoFieldNumber
+
+
+info_test_data = [
+    (
+        ["INTFIELD", 100, InfoFieldNumber.one],
+        "INTFIELD=100"
+    ),
+    (
+        ["FLOATFIELD", 100.0, InfoFieldNumber.one],
+        "FLOATFIELD=100.0"
+    ),
+    (
+        ["CHARFIELD", "C", InfoFieldNumber.one],
+        "CHARFIELD=C"
+    ),
+    (
+        ["STRINGFIELD", "hello", InfoFieldNumber.one],
+        "STRINGFIELD=hello"
+    ),
+    (
+        ["FLAG", True, InfoFieldNumber.one, True],
+        "FLAG"
+    ),
+    (
+        ["FLAG", False, InfoFieldNumber.one, True],
+        ""
+    ),
+    (
+        ["MULTIINTFIELD", [100, 100], InfoFieldNumber.A],
+        "MULTIINTFIELD=100,100"
+    ),
+    (
+        ["MULTIFLOATFIELD", [100.0, 100.0], InfoFieldNumber.A],
+        "MULTIFLOATFIELD=100.0,100.0"
+    ),
+    (
+        ["MULTICHARFIELD", ["C", "C"], InfoFieldNumber.A],
+        "MULTICHARFIELD=C,C"
+    ),
+    (
+        ["MULTISTRINGFIELD", ["foo", "bar"], InfoFieldNumber.A],
+        "MULTISTRINGFIELD=foo,bar"
+    )
+]
 
 
 vcf_test_data = [
@@ -30,8 +74,32 @@ vcf_test_data = [
     (
         ["1", 100, "A", ["T,C"], 500, ["DN1", "DN2"]],
         "1\t100\t.\tA\tT,C\t500\tDN1,DN2"
+    ),
+    (
+        ["1", 100, "A", ["T,C"], 500, ["DN1", "DN2"], "rsUnknown",
+         [InfoField("FOO", "bar", InfoFieldNumber.one)]],
+        "1\t100\trsUnknown\tA\tT,C\t500\tDN1,DN2\tFOO=bar"
+    ),
+    (
+        ["1", 100, "A", ["T,C"], 500, ["DN1", "DN2"], "rsUnknown",
+         [
+             InfoField("FOO", "bar", InfoFieldNumber.one),
+             InfoField("BAZ", True, InfoFieldNumber.one, True)
+         ]],
+        "1\t100\trsUnknown\tA\tT,C\t500\tDN1,DN2\tFOO=bar;BAZ"
     )
 ]
+
+
+@pytest.mark.parametrize("info_args, expected_str", info_test_data)
+def test_info_str(info_args, expected_str):
+    a = InfoField(*info_args)
+    assert str(a) == expected_str
+
+
+def test_info_flag_exc():
+    with pytest.raises(ValueError):
+        InfoField("FLAG", "notaboolean", InfoFieldNumber.one, True)
 
 
 @pytest.mark.parametrize("vcf_args, expected_line", vcf_test_data)
