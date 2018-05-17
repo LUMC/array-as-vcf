a07209e2ced86b21b91811908ef82f06d7998368
Sander Bollen <a.h.b.bollen@lumc.nl>
2018-05-17 14:09:04 +0200

    lumi readers

--- aav/readers.py
+++ aav/readers.py
@@ -8,12 +8,13 @@ aav.readers
 """
 from math import log10
 from pathlib import Path
-from typing import Optional
+from typing import Optional, List
 
 from werkzeug.exceptions import NotFound
 
 from .variation import Variant, InfoFieldNumber, InfoField, Genotype
 from .lookup import RSLookup
+from .utils import comma_float
 
 
 GRCH37_LOOKUP = RSLookup("GRCh37")
@@ -178,3 +179,93 @@ class CytoScanReader(Reader):
         if confidence == 0:
             return 0
         return -10 * log10(confidence)
+
+
+class LumiReader(Reader):
+    """
+    Lumi readers have one header line.
+    They required rsID lookups.
+
+    The first two columns (rs id and chr) may be switched around
+    """
+
+    def __init__(self, path: Path,
+                 lookup_table: RSLookup,
+                 chr_prefix: Optional[str] = None,
+                 qual=100):
+        super().__init__(path, n_header_lines=1)
+        self.lookup_table = lookup_table
+        self.chr_prefix = chr_prefix
+        self.qual = qual
+
+    def __next__(self) -> Variant:
+        line_items = next(self.handle).strip().split("\t")
+        rs_id = self.get_rs_id(line_items)
+        raw_chrom = self.get_raw_chrom(line_items)
+        chrom = self.get_chrom(raw_chrom)
+        pos = int(line_items[2])
+        g_type = line_items[3]
+        gt = self.get_genotype(g_type)
+
+        infos = [
+            InfoField(
+                "Log_R_Ratio", comma_float(line_items[4]), InfoFieldNumber.one
+            ),
+            InfoField(
+                "CNV_Value", int(line_items[5]), InfoFieldNumber.one
+            ),
+            InfoField(
+                "Allele_Freq", comma_float(line_items[6]), InfoFieldNumber.one
+            )
+        ]
+
+        try:
+            ref, alt = self.lookup_table[rs_id]
+        except NotFound:
+            ref = "."
+            alt = "."
+
+        return Variant(chrom=chrom, pos=pos, ref=ref, alt=alt,
+                       qual=self.qual, id=rs_id, info_fields=infos,
+                       genotype=gt)
+
+    def get_chrom(self, chrom: str) -> str:
+        if self.chr_prefix is None:
+            return chrom
+        return "{0}{1}".format(self.chr_prefix, chrom)
+
+    def get_rs_id(self, line_items: List[str]) -> str:
+        raise NotImplementedError
+
+    def get_raw_chrom(self, line_items: List[str]) -> str:
+        raise NotImplementedError
+
+    def get_genotype(self, g_type: str) -> Genotype:
+        if g_type == "NC":
+            return Genotype.unknown
+        elif len(set(g_type)) == 2:
+            return Genotype.het
+        elif g_type == "AA":
+            return Genotype.hom_ref
+        elif g_type == "BB":
+            return Genotype.hom_alt
+        else:
+            return Genotype.unknown
+
+
+class Lumi370kReader(LumiReader):
+
+    def get_rs_id(self, line_items: List[str]) -> str:
+        return line_items[0]
+
+    def get_raw_chrom(self, line_items: List[str]) -> str:
+        return line_items[1]
+
+
+class Lumi317Reader(LumiReader):
+
+    def get_rs_id(self, line_items: List[str]) -> str:
+        return line_items[1]
+
+    def get_raw_chrom(self, line_items: List[str]) -> str:
+        return line_items[0]
---
+++ aav/utils.py
@@ -0,0 +1,22 @@
+"""
+aav.utils
+~~~~~~~~~
+
+:copyright: (c) 2018 Sander Bollen
+:copyright: (c) 2018 Leiden University Medical Center
+:license: MIT
+"""
+
+
+def comma_float(val: str) -> float:
+    """
+    Get float for a string that may contain commas in stead of dots
+    :param val: the value to be casted to float
+    :return: float
+    """
+    if "," in val and "." not in val:
+        return float(val.replace(",", "."))
+    elif "." in val:
+        return float(val)
+    else:
+        raise ValueError("Cannot parse string with both commas and dots")
