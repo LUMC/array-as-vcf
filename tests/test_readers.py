cf5dedf0ff3aa6622fa7c9c2d2f653274e3acb38
Sander Bollen <a.h.b.bollen@lumc.nl>
2018-05-17 17:15:11 +0200

    test genotypes. have to call the fixture functions directly here to be able to parametrize them.

--- tests/test_readers.py
+++ tests/test_readers.py
@@ -12,30 +12,57 @@ import pytest
 from aav.readers import (AffyReader, CytoScanReader,
                          Lumi317Reader, Lumi370kReader)
 from aav.lookup import RSLookup
+from aav.variation import Genotype
+
+
+@pytest.fixture(scope="module")
+def grch37_lookup():
+    return RSLookup(build="GRCh37", request_tries=3)
 
 
 @pytest.fixture
-def lumi_317_reader():
+def lumi_317_reader(grch37_lookup):
     p = Path(__file__).parent / Path("data") / Path("lumi_317_test.txt")
-    return Lumi317Reader(p, RSLookup(build="GRCh37", request_tries=3))
+    return Lumi317Reader(p, grch37_lookup)
 
 
 @pytest.fixture
-def lumi_370_reader():
+def lumi_370_reader(grch37_lookup):
     p = Path(__file__).parent / Path("data") / Path("lumi_370_test.txt")
-    return Lumi370kReader(p, RSLookup(build="GRCh37", request_tries=3))
+    return Lumi370kReader(p, grch37_lookup)
 
 
 @pytest.fixture
-def affy_reader():
+def affy_reader(grch37_lookup):
     p = Path(__file__).parent / Path("data") / Path("affy_test.txt")
-    return AffyReader(p, RSLookup(build="GRCh37", request_tries=3))
+    return AffyReader(p, grch37_lookup)
 
 
 @pytest.fixture
-def cytoscan_reader():
+def cytoscan_reader(grch37_lookup):
     p = Path(__file__).parent / Path("data") / Path("cytoscan_test.txt")
-    return CytoScanReader(p, RSLookup(build="GRCh37", request_tries=3))
+    return CytoScanReader(p, grch37_lookup)
+
+
+_grch37_lookup = grch37_lookup()
+genotype_test_data = [
+    (
+        affy_reader(_grch37_lookup),
+        [Genotype.unknown, Genotype.hom_ref, Genotype.het, Genotype.hom_alt]
+    ),
+    (
+        cytoscan_reader(_grch37_lookup),
+        [Genotype.hom_alt, Genotype.hom_ref, Genotype.het]
+    ),
+    (
+        lumi_317_reader(_grch37_lookup),
+        [Genotype.hom_alt, Genotype.hom_ref, Genotype.het, Genotype.unknown]
+    ),
+    (
+        lumi_370_reader(_grch37_lookup),
+        [Genotype.hom_alt, Genotype.hom_ref, Genotype.het, Genotype.unknown]
+    )
+]
 
 
 def test_affy_reader_amount(affy_reader):
@@ -52,3 +79,9 @@ def test_lumi317_reader_amount(lumi_317_reader):
 
 def test_lumi370_reader_amount(lumi_370_reader):
     assert len(list(lumi_370_reader)) == 4
+
+
+@pytest.mark.parametrize("reader, genotypes", genotype_test_data)
+def test_reader_genotypes(reader, genotypes):
+    found_gt = [x.genotype for x in reader]
+    assert found_gt == genotypes
