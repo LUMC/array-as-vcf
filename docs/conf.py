0de59d42432d87cfc60db1586d9492fd08a997bd
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-07-11 11:12:54 +0200

    Set documentation parameters dynamically

--- docs/conf.py
+++ docs/conf.py
@@ -15,18 +15,26 @@
 # import os
 # import sys
 # sys.path.insert(0, '/home/ahbbollen/KG/aav/aav')
+from distutils.dist import DistributionMetadata
+from pathlib import Path
 
+import pkg_resources
 
 # -- Project information -----------------------------------------------------
 
-project = 'aav'
-copyright = '2018, Author'
-author = 'Author'
+# Get package information from the installed package.
+package = pkg_resources.get_distribution("array_as_vcf")
+metadata_file = Path(package.egg_info) / Path(package.PKG_INFO)
+metadata = DistributionMetadata(path=str(metadata_file))
+
+project = 'python-isal'
+copyright = '2020, Leiden University Medical Center'
+author = 'Leiden University Medical Center'
 
 # The short X.Y version
-version = ''
+version = package.parsed_version.base_version
 # The full version, including alpha/beta/rc tags
-release = ''
+release = package.version
 
 
 # -- General configuration ---------------------------------------------------
